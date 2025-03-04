7f1edf3f9ffc53aa02f75b71c001b495f97479d5
MF <mfregwork21@gmail.com>
2025-03-04 10:04:19 -0800

    Navbar styling done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,22 +6,22 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img src={assets.logo} alt="logo" className='w-44 cursor-pointer'/>
-        <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink>
+        <ul className='md:flex items-start gap-5 font-medium'>
+            <NavLink to='/' >
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/doctors'>
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/about'>
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/contact'>
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
         <div>
