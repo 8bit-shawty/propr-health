9c345da8bf3b36ec6833313ff02aea634e79f70e
MF <mfregwork21@gmail.com>
2025-03-04 10:28:36 -0800

    Navbar functionality complete

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/* ----LEFT SIDE---- */}
+        <div>
+
+        </div>
+        {/* ----RIGHT SIDE---- */}
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
