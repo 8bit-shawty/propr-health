e5ddf20114c18d5e6ad1faf0dabc289c455588ad
MF <mfregwork21@gmail.com>
2025-03-05 11:51:20 -0800

    Related doctors functionality implemented

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 // import { Timestamp } from 'mongodb'
 
 const Appointment = () => {
@@ -128,6 +129,9 @@ const Appointment = () => {
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
+
+      {/*Listing related doctors */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
