c79f81b25f0c6266ed44f41fc3f9355953451678
MF <mfregwork21@gmail.com>
2025-03-06 17:13:11 -0800

    Initialized Backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
 
     const [showMenu, setShowMenu] = useState(false)
     const[token, setToken] = useState(true)
+    const[isOpen, setIsOpen] = useState(false)
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img onClick={()=>navigate('/')} src={assets.logo} alt="logo" className='w-44 cursor-pointer'/>
@@ -31,10 +32,12 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
             {
                 token 
-                ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                ? <div className='flex items-center gap-2 cursor-pointer group relative'
+                onClick={()=>setIsOpen(!isOpen)} //toggle dropdown on click
+                >
                     <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
                     <img className='w-2,5' src={assets.dropdown_icon} alt="" />
-                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                    <div className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 ${isOpen?'block':'hidden'} group-hover:block`}>
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                             <p onClick={() =>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                             <p onClick={() =>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p> 
