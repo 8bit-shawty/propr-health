cddd3e339e1d2c296b1a9cbd0812c40636631c5f
MF <mfregwork21@gmail.com>
2025-03-10 23:39:51 -0700

    Successfully booked doc appointment using UI

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -166,4 +166,4 @@ const bookAppointment = async (req,res) => {
     }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile}
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment}
