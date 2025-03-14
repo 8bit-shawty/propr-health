b98575fd19795ced8886007799393c7093c72533
MF <mfregwork21@gmail.com>
2025-03-13 21:31:35 -0700

    Added logic for doctor login

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import {assets} from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 
 const Login = () => {
     const[state, setState] = useState('Admin')
@@ -11,6 +12,7 @@ const Login = () => {
     const[password, setPassword] = useState('')
 
     const{setAToken, backendUrl} = useContext(AdminContext)
+    const{setDToken} = useContext(DoctorContext)
 
     const onSubmitHandler = async (e) => {
         e.preventDefault()
@@ -27,6 +29,15 @@ const Login = () => {
                 }
             }else{
                 
+                const {data} = await axios.post(backendUrl+'/api/doctor/login',{email,password})
+                if(data.success){
+                    // console.log(data.token);
+                    localStorage.setItem('dToken', data.token)
+                    setDToken(data.token)
+                    console.log(data.token)
+                }else{
+                    toast.error(data.message)
+                }
             }
         } catch (error) {
             
